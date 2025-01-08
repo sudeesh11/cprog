7db5a0439417dce94c9a1b237269f9b6c285719d
sudeesh08 <dineshacharya2032@gmail.com>
2025-01-08 19:24:32 +0545

    Create lab2.8.c

---
+++ lab2.8.c
@@ -0,0 +1,13 @@
+//Write a program to print the size of int, char, float, double and long double data types in C
+
+#include <stdio.h>
+
+int main() {
+    printf("Size of int: %zu bytes\n", sizeof(int));
+    printf("Size of char: %zu bytes\n", sizeof(char));
+    printf("Size of float: %zu bytes\n", sizeof(float));
+    printf("Size of double: %zu bytes\n", sizeof(double));
+    printf("Size of long double: %zu bytes\n", sizeof(long double));
+
+    return 0;
+}
