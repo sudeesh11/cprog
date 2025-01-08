c8730e3392507909046421e6f8fe13f90af300bf
sudeesh08 <dineshacharya2032@gmail.com>
2025-01-08 19:13:27 +0545

    Create lab2.2.c

---
+++ lab2.2.c
@@ -0,0 +1,16 @@
+
+/*Write a program to declare two integer and one float variables then
+initialize them to 10, 15, and 12.6. Also print the variable values in
+the screen.*/
+
+#include<stdio.h>
+int main(){
+	int a=15,b=10;
+	float c=12.6;
+	printf("Value of integer a: %d\n", a);
+    printf("Value of integer b: %d\n", b);
+    printf("Value of float c: %f\n", c);
+
+    getch();
+	return 0;
+}
