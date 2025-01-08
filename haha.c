47303e39e3804ede807017a6b478eb863656932f
sudeesh08 <dineshacharya2032@gmail.com>
2025-01-08 14:04:29 +0545

    Create haha.c

---
+++ haha.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+	char name[20];
+	int roll ;
+	printf("Enter your name and roll number");
+	scanf("%s %d",name, &roll);
+	printf("%s is your name and %d is your roll number.",name, roll);
+}
