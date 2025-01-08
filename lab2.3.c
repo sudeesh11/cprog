d196c34c73184796a6332a6bd5e8f30c68d465f7
sudeesh08 <dineshacharya2032@gmail.com>
2025-01-08 19:14:43 +0545

    Create lab2.3.c

---
+++ lab2.3.c
@@ -0,0 +1,14 @@
+/*Write a C program to prompt the user to input 3 integer values and
+print these values in forward and reversed order.*/
+#include<stdio.h>
+int main(){
+	int a,b,c;
+	
+	printf("Enter three numbers:");
+	scanf("%d %d %d", &a , &b, &c);
+	
+	/* printf("In forward order ");
+	printf("%d %d %d", a , b, c); */
+	printf("In reversed order ");
+	printf("%d %d %d", c, b, a);
+}
