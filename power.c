b0f8ec720c71311f53a5643b9b8004d499428a34
sudeesh11 <dineshacharya2032@gmail.com>
2025-01-24 20:45:07 +0545

    Create power.c

---
+++ power.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int power(int a,int b)
+{
+	if (b==0) {
+		return 1;
+	}
+	else {
+		return a*pow(a,b-1);
+	}
+}
+int main () {
+int a,b,c;
+printf("Enter a and b");
+scanf("%d %d",&a,&b);
+c=power(a,b);
+printf("The value of %d to the power %d is %d",a,b,c);
+}
