d8370096f57a45011f163691955991a24476dcef
sudeesh11 <dineshacharya2032@gmail.com>
2025-01-28 16:38:12 +0545

    Update lab2.7.c

--- lab2.7.c
+++ lab2.7.c
@@ -15,37 +15,37 @@ int main() {
     // a. y = x++ + ++x;
     x = 2; 
     y = x++ + ++x; 
-    printf("a. y = %d, x = %d\n", y, x); // Output: a. y = 6, x = 4
+    printf("a. y = %d, x = %d\n", y, x); 
 
     // b. y=++x + ++x;
     x = 2; 
     y = ++x + ++x; 
-    printf("b. y = %d, x = %d\n", y, x); // Output: b. y = 7, x = 4
+    printf("b. y = %d, x = %d\n", y, x); 
 
     // c. y= ++x + ++x + ++x; 
     x = 2; 
     y = ++x + ++x + ++x; 
-    printf("c. y = %d, x = %d\n", y, x); // Output: c. y = 12, x = 5
+    printf("c. y = %d, x = %d\n", y, x);
 
     // d. y = x>z; 
     x = 2; 
     y = x>z; 
-    printf("d. y = %d, x = %d, z = %d\n", y, x, z); // Output: d. y = 0, x = 2, z = 4
+    printf("d. y = %d, x = %d, z = %d\n", y, x, z);
 
     // e. y= x>z? x:z; 
     x = 2; 
     y = x>z? x:z; 
-    printf("e. y = %d, x = %d, z = %d\n", y, x, z); // Output: e. y = 4, x = 2, z = 4
+    printf("e. y = %d, x = %d, z = %d\n", y, x, z);
 
     // f. y = x&z;
     x = 2; 
     y = x&z; 
-    printf("f. y = %d, x = %d, z = %d\n", y, x, z); // Output: f. y = 0, x = 2, z = 4
+    printf("f. y = %d, x = %d, z = %d\n", y, x, z); 
 
     // g. y= x>>2 + z<<1;
     x = 2; 
     y = x>>2 + z<<1; 
-    printf("g. y = %d, x = %d, z = %d\n", y, x, z); // Output: g. y = 8, x = 2, z = 4
+    printf("g. y = %d, x = %d, z = %d\n", y, x, z); 
 
     return 0;
 }
