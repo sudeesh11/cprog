249a1994f658e0706e30af391a8c6d6165abf042
sudeesh11 <dineshacharya2032@gmail.com>
2025-02-20 13:32:23 +0545

    Update

---
+++ computer.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main()
+{
+int a;
+float b;
+double c;
+char d;
+printf("Size of int: %d bytes\n",sizeof(a));
+printf("Size of float: %d bytes\n",sizeof(b));
+printf("Size of double: %d bytes\n",sizeof(c));
+printf("Size of char: %d byte\n",sizeof(d));
+system("pause");
+return (0);
+}
