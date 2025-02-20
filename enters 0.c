eb1b40a8f48c4540afe07a81f215007336aa0065
sudeesh11 <dineshacharya2032@gmail.com>
2025-02-20 15:10:22 +0545

    Create enters 0.c

---
+++ enters 0.c
@@ -0,0 +1,14 @@
+int main()
+{
+int sum=0,num;
+do/* Codes inside the body of do...while loops are at least
+executed once.*/
+{
+printf("Enter a number ="); scanf("%d",&num);
+sum+=num;
+}
+while(num!=0); printf("sum=%d\n",sum); system("pause");
+ return 0;
+  
+ }
+ 
