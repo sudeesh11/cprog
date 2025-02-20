6318cee69cc16a1cd3a34aa0fd0a517051711f30
sudeesh11 <dineshacharya2032@gmail.com>
2025-02-20 21:21:20 +0545

    Update lab6.6.c

--- lab6.6.c
+++ lab6.6.c
@@ -1,18 +1,24 @@
 #include <stdio.h>
-
+int c = 5;
 void function() {
   int a = 10;
+  a++;
   printf("Local: %d\n", a);
+  a++;
 
   static int b = 0;
+
   b++;
   printf("Static: %d\n", b);
+  b++;
+  
+  c++;
+  printf("Global: %d\n", c);
+  c++;
 }
 
-int c = 5;
 
 int main() {
-  printf("Global: %d\n", c);
 
   function();
   function();
