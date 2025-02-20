3218f060b371e6994779cab9496bd9d414757946
sudeesh11 <dineshacharya2032@gmail.com>
2025-02-20 16:54:47 +0545

    Create 11 12 13 21 31 32 33.c

---
+++ 11 12 13 21 31 32 33.c
@@ -0,0 +1,13 @@
+
+#include <stdio.h>
+int main() {
+    for (int i = 1; i <= 3; i++) {
+        for (int j = 1; j <= 3; j++) {
+            if (i == 2 && j == 2) {
+                break;
+            }
+            printf("%d,%d\n", i, j);
+        }
+    }
+    return 0;
+}
