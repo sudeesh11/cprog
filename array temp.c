0397a074930e302d3396fa2fd5b83b03beb8fae0
sudeesh11 <dineshacharya2032@gmail.com>
2025-02-20 17:27:30 +0545

    Create array temp.c

---
+++ array temp.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main() {
+    int arr[5] = {5, 3, 4, 1, 2};
+    for (int i = 0; i < 4; i++) {
+        for (int j = 0; j < 4 - i; j++) {
+            if (arr[j] > arr[j + 1]) {
+                int temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
+    }
+    for (int i = 0; i < 5; i++) {
+        printf("%d ", arr[i]);
+    }
+    return 0;
+}
