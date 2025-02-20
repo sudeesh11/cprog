f84bc1de270bf5111b79b625baeab4cbdf690411
sudeesh11 <dineshacharya2032@gmail.com>
2025-02-20 17:17:53 +0545

    Create number pattern4.c

---
+++ number pattern4.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+  int main()
+{
+int i,j,k;
+for(i=5;i>=1;i--)
+{
+if(i%2==1) k=1;
+else k=i;
+for(j=1;j<=i;j++)
+{
+printf("%d",k);
+if(i%2==1) k++;
+else k--;
+}
+printf("\n\n");
+}
+system("pause");
+return 0;
+}
