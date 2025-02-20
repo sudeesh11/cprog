d53bb3d42ea6b9baf7e3de6a3f0594fc9a92f8f2
sudeesh11 <dineshacharya2032@gmail.com>
2025-02-20 16:35:48 +0545

    Create frequency.c

---
+++ frequency.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+int main()
+{
+int n, t, i, j, arr[100],len, halflen,flag=0,count=0;printf("Enter number of elements to insert in an array:");scanf("%d",&len);
+printf("Enter elements to insert in an array:\n");for(i=0;i<len;i++){
+scanf("%d",&t);
+arr[i]=t;
+}
+printf("\n");
+for(i=0;i<len;i++){
+count=1;
+for(j=i+1;j<=len-1;j++){
+if(arr[i]==arr[j] && arr[i]!='\0'){
+count++;
+arr[j]='\0';
+}
+}
+if(arr[i]!='\0'){
+printf("%d is repeated %d times.\n",arr[i],count);}
+}
+getch();
+return 0;
+}
