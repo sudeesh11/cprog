b8ae943e466b553bcc125b15bf0f6b41b428f1e3
sudeesh11 <dineshacharya2032@gmail.com>
2025-02-20 13:34:46 +0545

    Update

---
+++ largest.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main()
+{
+float a, b, c;
+printf("Enter three numbers a b c: ");
+scanf("%f %f %f",&a,&b,&c);
+if(a>=b && a>=c)
+printf("Largest number is a =%f\n",a);
+if(b>=a && b>=c)
+printf("Largest number is b=%f\n",b);
+if(c>=a && c>=b)
+printf("Largest number is c=%f\n",c);
+system("pause");
+return 0;
+}
