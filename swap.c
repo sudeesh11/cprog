29e356132e640996c8d47ec7236f56c851ef8824
sudeesh11 <dineshacharya2032@gmail.com>
2025-02-20 13:31:50 +0545

    Update

---
+++ swap.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main()
+{
+float a,b,temp;
+printf("Enter value of a: ");
+scanf("%f",&a);
+printf("Enter value of b: ");
+scanf("%f",&b);
+temp=a; /*Value of a is stored in variable temp */
+a=b; /*Value of b is stored in variable a*/
+b=temp;/*Value of temp(which contains initial value of a) is
+stored in variable b*/
+printf("After swapping,value of a = %f\n",a);
+printf("After swapping,value of b = %f\n",b);
+system("pause");
+return (0);
+}
