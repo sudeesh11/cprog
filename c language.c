0d55b992140dff585574c2f627386dbbf756faa6
sudeesh11 <dineshacharya2032@gmail.com>
2025-02-20 14:59:04 +0545

    Create c language.c

---
+++ c language.c
@@ -0,0 +1,20 @@
+
+#include<stdio.h>
+int main(){
+  int num,temp,factor=1;
+  printf("Enter a number: ");
+  scanf("%d",&num);
+  temp=num;
+  while(temp){
+     temp=temp/10;
+     factor = factor*10;
+  }
+  printf("Each digits of given number are: \n");
+  while(factor>1){
+     factor = factor/10;
+     printf("%d ",num/factor);
+   num = num % factor;
+  }
+system("pause");
+return 0;
+}
